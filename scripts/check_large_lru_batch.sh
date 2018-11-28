c52f95eac10d65f65c03897a114a102cddae5ad0
Benjamin Garrett <bgarrett@temple.edu>
2018-11-28 07:38:55 -0500

    Stable, major refactoring

---
+++ scripts/check_large_lru_batch.sh
@@ -0,0 +1,40 @@
+
+
+# obtain event log
+p1=../../memo_batch_tester/dist/Debug/GNU-Linux/memo_batch_tester
+ptp=--mbt_problem_type
+pt=test
+osp=--mbt_operation_sequence
+os=../../misc_phd/input/operation_sequences/without_feedback/without_deletions/operation_sequence_without_feedback-100000-1000000-balanced
+csp=--memo_caching_strategy
+cs=lru
+czp=--memo_cache_size
+cz=65536
+klp=--memo_key_length
+kl=8
+vlp=--memo_value_length
+vl=8
+ckp=--memo_cuckoo_k
+ck=4
+lqp=--memo_lru_queue_size
+lq=7
+elfp=--memo_event_log_fname
+elf=event_log
+$p1 $ptp $pt $osp $os $csp $cs $czp $cz $klp $kl $vlp $vl $ckp $ck $lqp $lq $elfp $elf
+
+
+mv event_log event_log_
+
+# check ages of evictions
+p2=../dist/Debug/GNU-Linux/cache_eviction_analyzer
+rsp=--cea_result_sequence
+rs=event_log_
+csp=--memo_caching_strategy
+cs=linear_probe_with_deletions
+czp=--memo_cache_size
+cz=100000
+klp=--memo_key_length
+kl=8
+vlp=--memo_value_length
+vl=8
+$p2 $rsp $rs $csp $cs $czp $cz $klp $kl $vlp $vl
---
+++ scripts/run_local_event_log.sh
@@ -0,0 +1,4 @@
+p=../dist/Debug/GNU-Linux/cache_eviction_analyzer
+rsp=--cea_result_sequence
+rs=event_log
+$p $rsp $rs
